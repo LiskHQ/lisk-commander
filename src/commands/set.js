d5f333b152b3d3ba22cf50c813bc38d1ba6624c0
willclarktech <willclarktech@users.noreply.github.com>
2017-07-28 13:59:56 +0200

    Refactor set command

--- src/commands/set.js
+++ src/commands/set.js
@@ -1,44 +1,47 @@
 import fse from 'fs-extra';
 import config from '../../config.json';
 
-const checkBoolean = value => ['true', 'false'].includes(value);
-
 const writeConfigToFile = (newConfig) => {
 	const configString = JSON.stringify(newConfig, null, '\t');
 	fse.writeFileSync('config.json', `${configString}\n`, 'utf8');
 };
 
+const checkBoolean = value => ['true', 'false'].includes(value);
+
+const setBoolean = (variable, path) => (value) => {
+	if (!checkBoolean(value)) {
+		return { message: `Cannot set ${variable} to ${value}.` };
+	}
+	const newValue = (value === 'true');
+	path
+		.reduce((obj, pathComponent, i) => {
+			if (i === path.length - 1) {
+				// eslint-disable-next-line no-param-reassign
+				obj[pathComponent] = newValue;
+				return config;
+			}
+			return obj[pathComponent];
+		}, config);
+
+	writeConfigToFile(config);
+	return { message: `Successfully set ${variable} to ${value}.` };
+};
+
+const set = (userInput, callback) => {
+	const getType = {
+		json: setBoolean('json output', ['json']),
+		testnet: setBoolean('testnet', ['liskJS', 'testnet']),
+	};
+
+	const returnValue = Object.keys(getType).includes(userInput.variable)
+		? getType[userInput.variable](userInput.value)
+		: { message: 'Unsupported variable name.' };
+	return (callback && typeof callback === 'function') ? callback(returnValue.message) : returnValue.message;
+};
+
 export default function setCommand(vorpal) {
-	function setJSON(value) {
-		if (!checkBoolean(value)) {
-			return { message: `Cannot set json to ${value}.` };
-		}
-		config.json = (value === 'true');
-		writeConfigToFile(config);
-		return { message: `Successfully set json output to ${value}.` };
-	}
-
-	function setTestnet(value) {
-		if (!checkBoolean(value)) {
-			return { message: `Cannot set testnet to ${value}.` };
-		}
-		config.liskJS.testnet = (value === 'true');
-		writeConfigToFile(config);
-		return { message: `Successfully set testnet to ${value}.` };
-	}
-
 	vorpal
 		.command('set <variable> <value>')
 		.description('Set configuration <variable> to <value>.')
-		.action((userInput, callback) => {
-			const getType = {
-				json: setJSON,
-				testnet: setTestnet,
-			};
-
-			const returnValue = Object.keys(getType).includes(userInput.variable)
-				? getType[userInput.variable](userInput.value)
-				: { message: 'Unsupported variable name.' };
-			return (callback && typeof callback === 'function') ? callback(returnValue.message) : returnValue.message;
-		});
+		.action(set);
 }
